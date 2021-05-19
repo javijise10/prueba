303e8129ea9a559c65e5d594233ecff0a6b42517
javijise10 <83302972+javijise10@users.noreply.github.com>
2021-05-19 17:45:25 +0200

    Update Ejemplo.java

--- ejemplo/src/ejemplo/Ejemplo.java
+++ ejemplo/src/ejemplo/Ejemplo.java
@@ -16,7 +16,7 @@ public class Ejemplo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("HOLA JAVIER");
+        System.out.println("HOLA Juan");
     }
     
 }
